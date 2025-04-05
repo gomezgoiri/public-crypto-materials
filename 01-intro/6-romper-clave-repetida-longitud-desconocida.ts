9a49cfb09283659e5b03d57fa8a5f041bb634e64
Aitor Gomez-Goiri <aitor.gomez@tecnalia.com>
2025-04-05 11:37:39 +0200

    Añadiendo más información para realizar el ejercicio 6 de la unidad introductoria

--- 01-intro/6-romper-clave-repetida-longitud-desconocida.ts
+++ 01-intro/6-romper-clave-repetida-longitud-desconocida.ts
@@ -52,7 +52,60 @@ import { Buffer } from "node:buffer"
 function hammingDistance(buff1: Buffer, buff2: Buffer): number {
 }
 
+/**
+ * Esta función recibirá un tamaño de clave y devolverá una medida de
+ * la similitud de trozos de CT de ese tamaño.
+ *
+ * Casos que podeis usar para comprobar si vuestra implementación es correcta:
+ *    - candidateKeySize == 2:   1.5382978723404255
+ *    - candidateKeySize == 5:   1.5755319148936173
+ *    - candidateKeySize == 10:  1.5404255319148938
+ *    - candidateKeySize == 45:  1.3944444444444444
+ *    - candidateKeySize == 88:  1.5346590909090911
+ */
 function scoreKeySize(candidateKeySize: number, cipherText: Buffer): number {
+  // Troceamos en cachos el doble de grande que el candidateKeySize para sacar
+  // dos muestras por cada vuelta.
+  const sliceSize = 2 * candidateKeySize
+
+  let score = 0
+  const numSlices = 1 // ¿Cuantos trozos de tamaño "sliceSize" hay en Buffer?
+
+  for (let i = 0; i < numSlices; i++) {
+    // Extraer un trozos de tamaño sliceSize de "cipherText" y dividirlo en 2.
+    const slice1 = cipherText.subarray(0, 0)
+    const slice2 = cipherText.subarray(0, 0)
+
+    // Calculamos la hamming distance de los dos trozos
+    const partialScore = hammingDistance(slice1, slice2)
+
+    // Dos trozos de tamaño 5 tendrán potencialmente mas score que dos trozos
+    // de tamaño 10 porque hacen comparaciones entre más caracteres.
+    // Por ello, normalizamos partialScore para hacerlo independiente del
+    // tamaño de los trozos.
+    score = 0
+  }
+
+  // A  mayor "candidateKeySize", menos trozos podremos sacar del buffer
+  // y por tanto, menor score acumulado potencialmente.
+  // Necesitaremos normalizar la puntuación devuelta.
+  // Para ello, uniformizaremos el score entre llamadas dividiendo la
+  // puntuación total entre el número de trozos analizados.
+  return 0
+}
+
+// Troceo como el del ejercicio 5.
+// Si CT: 0a 0b 0c 0d 0e 0f 00 01 02 03 04
+// Y keySize: 3
+// Entonces debería devolver 3 Buffers:
+//    - 0a 0d 00 03
+//    - 0b 0e 01 04
+//    - 0c 0f 02
+function sliceVigenereCipherText(
+  cipherText: Buffer,
+  keySize: number,
+): Buffer[] {
+  return [Buffer.alloc(0)]
 }
 
 // Sacar mensaje cifrado de un fichero de texto.
@@ -67,17 +120,24 @@ for (let i = 2; i < 100; i++) {
 // Ordenamos de más a menos probables
 candidateSizes.sort((a, b) => a[1] - b[1])
 
-// Mostramos los mejores candidatos y elegimos uno
+// Calculamos las claves probables para los 5 tamaños mas probables.
+// Pista: podemos usar slice() para quedarnos con ellas.
+// Para cada una, seguimos el proceso del ejercicio 5:
+//   1. sliceVigenereCipherText
+//   2. findMostLikelyKeyForLanguage para cada letra de la clave
+//   3. Unimos las letras candidatas
 const bestCandidateKeys = []
 
+// Mostramos los mejores candidatas al usuario.
 console.log("Mejores claves candidatas:\n")
-bestCandidateKeys.slice(0, 5).forEach((key, i) => {
+bestCandidateKeys.forEach((key, i) => {
   console.log(
     `\t${i + 1}. "${key.toString()}" (size=${key.length})`,
   )
 })
 console.log()
 
+// El usuario elige una de esas claves candidatas.
 let choice
 do {
   const answer = prompt("Selecciona la mejor candidata:")
